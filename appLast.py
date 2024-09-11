9a937417ee06c14adf3d78d3d8983540497ea7ab
hemreozdes <hemreozdes@gmail.com>
2024-09-11 20:33:41 +0300

    Final

--- appLast.py
+++ appLast.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect,url_for,jsonify
+from flask import Flask,render_template,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='templates')
